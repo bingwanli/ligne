8c90d016128b0d9d0024f86107f3dba95a88b4fe
Gini Li <bingwan.gini.li@gmail.com>
2019-12-11 15:51:12 +0800

    added job to generate recommendations

--- app/jobs/fit_recs_job.rb
+++ app/jobs/fit_recs_job.rb
@@ -2,40 +2,17 @@ class FitRecsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # @recommender = Disco::Recommender.new(factors: 8)
-    fit_recs
-    save_recs
+    generate_recs
     puts "perform worked"
   end
 
   private
 
-  def convert_data
-    data = []
-    object_data = ProductRec.all
-    object_data.each do |h|
-      data << {
-        user_id: h["user_id"],
-        item_id: h["product_id"],
-        rating: h["rating"]
-      }
-    end
-    data
-  end
-
-  def fit_recs
-    data = convert_data
-    # @recommender.fit(data)
-    # returns an array of arrays of probabilities
-    puts "fitted recs"
-  end
-
-  def save_recs
-    User.find_each do |user|
-      # recs = @recommender.user_recs(user.id)
-      user.update_recommended_products(recs)
-      # returns array of hashes per user that has item id and score
-      puts "saved recs"
+  def generate_recs
+    users = User.all
+    users.each do |user|
+      Recommendable::Helpers::Calculations.update_similarities_for(user)
+      Recommendable::Helpers::Calculations.update_recommendations_for(user)
     end
   end
 end
