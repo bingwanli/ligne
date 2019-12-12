b872f7755c1c12c7c5ea292ce4b4c338407e6aea
Renee <renee.tangwanyi@gmail.com>
2019-12-12 15:19:40 +0800

    brand show

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 brand = Brand.create({ id: 1,
   name: 'Mara Hoffman',
   description: "Mara Hoffman founded her label in 2000 after graduating from Parsons School of Design in New York City. Fifteen years later, the brand committed itself to implementing more sustainable and responsible practices. In an effort to foster mindful consumption habits, the brand maintains an open conversation about its approach and encourages consumers to reevaluate the relationship society has with clothing. The company continues to focus on sustainable materials, processes, and production in order to improve and extend each garment’s life. President and Creative Director, Mara Hoffman, presents collections with a devotion to color, each inspired by and in celebration of women.",
-  country: 'USA',
+  country: 'US',
   logo_url: 'http://gfiny.com/wp-content/uploads/2016/02/mara-hoffman.jpg' })
 
 url = 'https://www.marahoffman.com/shop-all'
