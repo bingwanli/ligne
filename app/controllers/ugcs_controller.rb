c6afe23fd2e6abcd641e67c18904a9609e59172a
Gini Li <bingwan.gini.li@gmail.com>
2019-12-18 13:11:05 +0800

    added product recs page reformatted with brand and products below brands

--- app/controllers/ugcs_controller.rb
+++ app/controllers/ugcs_controller.rb
@@ -4,7 +4,7 @@ class UgcsController < ApplicationController
     @ugc.user = current_user
     respond_to do |format|
       if @ugc.save
-        format.html { redirect_to edit_user_registration_path(current_user), notice: 'Wardrobe updated!'}
+        format.html { redirect_to edit_user_registration_path, notice: 'Wardrobe updated!'}
       else
         format.html { render action: 'new' }
       end
@@ -12,6 +12,9 @@ class UgcsController < ApplicationController
   end
 
   def destroy
+    @ugc = Ugc.find(params[:id])
+    @ugc.delete
+    redirect_to edit_user_registration_path
   end
 
   private
